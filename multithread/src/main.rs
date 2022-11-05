c5f1be84f99f0b5fbb15f8ce6f140b848a860aac
Will Mortl <will@williammortl.com>
2022-11-05 09:25:27 -0600

    updates

--- multithread/src/main.rs
+++ multithread/src/main.rs
@@ -5,9 +5,11 @@ use text_io::read;
 
 fn main() {
     for i in 1..10 {
-        thread::spawn(move || {
+        //thread::spawn(move || {
+        thread::spawn(|| {
             for j in 1..20 {
-                println!("thread: {}, number: {}", i, j);
+                //println!("thread: {}, number: {}", i, j);
+                println!("thread");
             }
         });
     }
